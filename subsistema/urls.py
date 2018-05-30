f4e12a96d048f156b88621418b4d317fdebf0364
Lucas Velten Henker <32674592+lvhlucas@users.noreply.github.com>
2018-05-30 09:31:06 -0300

    Update urls.py

--- subsistema/urls.py
+++ subsistema/urls.py
@@ -4,11 +4,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'subsistema'
-urlpatterns = [           
-    path('login/', views.login, name='login'),    
-    #path('accounts/login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),    
-    #path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
-    path('login/cadastrar/', views.alunoCadastra, name='alunoCadastra'),       
+urlpatterns = [                  
+    path('cadastrar/', views.alunoCadastra, name='alunoCadastra'),       
     path('alunopedemonitor/', views.AlunoPedeMonitor, name='AlunoPedeMonitor'),       
 ]
